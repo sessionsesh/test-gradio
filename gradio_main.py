96015471bb1b49d4a956523e6f4a781e2bed40ec
sessionsesh <sessionsesh@gmail.com>
2024-05-20 16:05:26 +0300

    changes

--- gradio_main.py
+++ gradio_main.py
@@ -105,5 +105,4 @@ def read_main():
     return {"message": "This is your main app"}
 
 
-io = gr.Interface(lambda x: "Hello, " + x + "!", "textbox", "textbox")
-app = gr.mount_gradio_app(app, io, path="/gradio")
+app = gr.mount_gradio_app(app, demo, path="/gradio")
